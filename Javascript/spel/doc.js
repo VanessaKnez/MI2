be48a11fd55ad7e2dcc67cc0b05b7af555939866
VanessaKnez <32335826+VanessaKnez@users.noreply.github.com>
2018-05-02 22:09:16 +0200

    Add files via upload

--- Javascript/spel/doc.js
+++ Javascript/spel/doc.js
@@ -1,48 +1,94 @@
 "use strict";
+var windowheight,
+    windowwidth,
+    speler;
 /* jshint esnext: true */
 // met een lus ervoor zorgen dat de rode bolletjes 10 keer getoond worden
 $(document).ready(function () {
-        var i = 0;
-        for (i; i < 10; i++) {
-            Teken();
-        }
-    })
-    // hier gaan we de rode bolletjes tekenen
+    var i = 0;
+    for (i; i < 10; i++) {
+        Teken();
+    }
+    windowheight = window.innerHeight;
+    windowwidth = window.innerWidth;
+    speler = $(".speler");
+})
+// hier gaan we de rode bolletjes tekenen
 var Teken = () => {
-        var offsetleft = GetRandom(window.innerWidth);
-        var offsettop = GetRandom(window.innerHeight);
-        document.body.innerHTML += "<div class='vijand' style='left: " + offsetleft + "px; top: " + offsettop + "px'></div>";
-    }
-    //  hier zetten we een random positie
+    var offsetleft = GetRandom(window.innerWidth);
+    var offsettop = GetRandom(window.innerHeight);
+    document.body.innerHTML += "<div class='vijand' style='left: " + offsetleft + "px; top: " + offsettop + "px'></div>";
+}
+//  hier zetten we een random positie
 var GetRandom = (max) => {
-        return Math.floor(Math.random() * max) - 25;
-    }
-    // de speler laten bewegen met de pijltjes toetsen
+    return Math.floor(Math.random() * max) - 25;
+}
+// de speler laten bewegen met de pijltjes toetsen
 $(document).keydown(function (e) {
     switch (e.which) {
-    case 37:
-        //links
-        $(".speler").finish().animate({
-            left: "-=10"
-        });
-        break;
-    case 38:
-        //boven
-        $(".speler").finish().animate({
-            top: "-=10"
-        });
-        break;
-    case 39:
-        //rechts
-        $(".speler").finish().animate({
-            left: "+=10"
-        });
-        break;
-    case 40:
-        //beneden
-        $(".speler").finish().animate({
-            top: "+=10"
-        });
-        break;
+        case 37:
+            //links
+            if (!DetecteerCollisie("links"))
+                $(".speler").finish().animate({
+                    left: "-=10"
+                });
+            break;
+        case 38:
+            //boven
+            if (!DetecteerCollisie("boven"))
+                $(".speler").finish().animate({
+                    top: "-=10"
+                });
+            break;
+        case 39:
+            //rechts
+            if (!DetecteerCollisie("rechts"))
+                $(".speler").finish().animate({
+                    left: "+=10"
+                });
+            break;
+        case 40:
+            //beneden
+            if (!DetecteerCollisie("beneden"))
+                $(".speler").finish().animate({
+                    top: "+=10"
+                });
+            break;
     }
 });
+
+// als de speler tegen de rand is dan stoppen
+function DetecteerCollisie(richting) {
+    console.log("detecteercollisie")
+    switch (richting) {
+        case "boven":
+            console.log(speler.css("top"))
+            console.log(parseInt(speler.css("top")) - 10)
+            if ((parseInt(speler.css("top")) - 17) < 0) {
+
+                return true;
+            } else return false;
+            break;
+        case "links":
+            if ((parseInt(speler.css("left")) - 17) < 0) {
+                return true;
+            } else return false;
+            break;
+        case "rechts":
+            if ((parseInt(speler.css("left")) + 32.5) > windowwidth) {
+                return true;
+            } else return false;
+            break;
+        case "beneden":
+            if ((parseInt(speler.css("top")) + 38) > windowheight) {
+                return true;
+            } else return false;
+            break;
+
+    }
+}
+
+// score maken voor de speler
+function Score(){
+    var score = 0;
+}
